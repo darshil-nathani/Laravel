a39ec552c18582ebe52b643a4deca01b2fb221be
darshil-nathani <darshilnathani1412@gmail.com>
2024-03-11 07:32:14 -0700

    sidder

--- database/seeders/myblogSeeder.php
+++ database/seeders/myblogSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use \App\Models\myblog;
+use App\Models\myblog;
 
 class myblogSeeder extends Seeder
 {
