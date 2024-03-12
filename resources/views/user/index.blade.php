3084917c726a59bce3147af4f5a124aae7515e12
darshil-nathani <darshilnathani1412@gmail.com>
2024-03-12 06:39:28 -0700

    HOME PAGE

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,42 +1,60 @@
 @extends('template.layout')
+
 @push('title')
-Home Page
+    Home Page
 @endpush
 
 @section('content')
-<h1>My Home Page</h1>
-<div class="row">
-    <div class="col-md-8"></div>
-    <div class="col-md-4"><a href="{{route('mypost.create')}}" class="btn btn-primary">+ Post 📃</a></div>
-</div>
-<table class="table table-hover table-sm table-stripped">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Subtitle</th>
-            <th>View</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </tr>
-    </thead>
-    @foreach($myposts as $mypost)
-        <tr>
-            <td>{{$mypost->id}}</td>
-            <td>{{$mypost->title }}</td>
-            <td>{{$mypost->subtitle }}</td>
-            <td>
-                <a href="{{ route('mypost.show', $mypost->id) }}" class="btn btn-info">View Me! 👀</a>
-            </td>
-            <td><a href="{{ route('mypost.edit', $mypost->id) }}" class="btn btn-warning">Edit Me! ✒</a></td>
-            <td>
-                <form action="{{ route('mypost.destroy', $mypost->id) }}" method="post">
-                    @csrf
-                    @method('delete')
-                    <input type="submit" value="Delete Me! 💥" class="btn btn-danger"/>
-                </form>
-            </td>
-        </tr>
-    @endforeach
-</table>
+    <div class="container">
+        <h1 class="my-4">My Home Page</h1>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Title</th>
+                                        <th>Subtitle</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($myposts as $mypost)
+                                        <tr>
+                                            <td>{{$mypost->id}}</td>
+                                            <td>{{$mypost->title }}</td>
+                                            <td>{{$mypost->subtitle }}</td>
+                                            <td>
+                                                <div class="btn-group" role="group" aria-label="Post Actions">
+                                                    <a href="{{ route('mypost.show', $mypost->id) }}" class="btn btn-outline-info">View 👀</a>
+                                                    <a href="{{ route('mypost.edit', $mypost->id) }}" class="btn btn-outline-warning">Edit ✒</a>
+                                                    <form action="{{ route('mypost.destroy', $mypost->id) }}" method="post" class="d-inline">
+                                                        @csrf
+                                                        @method('delete')
+                                                        <button type="submit" class="btn btn-outline-danger" onclick="return confirm('Are you sure you want to delete this post?')">Delete 💥</button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Add New Post</h5>
+                        <p class="card-text">Create a new post to share your thoughts.</p>
+                        <a href="{{route('mypost.create')}}" class="btn btn-primary btn-block">+ Add Post 📃</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
